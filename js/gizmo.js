8db049b81084a472b3c3c189d8ac501daed8cad1
Dan Morrison <danielroy.morrison@gmail.com>
2015-04-22 20:04:45 +1200

    content styled and ajax call added

--- js/gizmo.js
+++ js/gizmo.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-  
+
+  output = $("#output");
+
+  if (window.innerWidth <= 480){
+    $("#submit").css("margin-top", "20px");
+  }
+
+  $("#testForm").submit(function(e){
+    e.preventDefault();
+    var $form = $(this),
+        user = $form.find("input[name='userName']").val(),
+        pass = $form.find("input[name='password']").val(),
+        url = $form.attr("action");
+
+    $.ajax({
+      type:"POST",
+      url:url,
+      data:{"form":{ "username": user,
+                   "password": pass }},
+      dataType:"json",
+      success: function(data){
+        $("#userOutput").empty()
+        $("#userPass").empty()
+        $("#userOutput").append(data["results:"].username);
+        $("#userPass").append(data["results:"].password);
+      }
+    })
+  });
 });
